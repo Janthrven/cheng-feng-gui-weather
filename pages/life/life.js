436dbd428c10b32b953048e6b13ffbc73e8e049d
Janthrven <894685667@qq.com>
2021-11-04 16:18:20 +0800

    第一次提交

---
+++ pages/life/life.js
@@ -0,0 +1,100 @@
+// pages/life/life.js
+const app = getApp();
+let globalData = app.globalData;
+let utils = require('../../utils/utils.js');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    poster:'https://img1.baidu.com/it/u=3791044796,1935071913&fm=26&fmt=auto',
+    //生活指数列表
+    lifeList: [],
+  },
+
+   //初始化
+   init() {
+    this.getLifeList();
+  },
+
+  //生活指数
+  getLifeList() {
+    wx.showLoading({
+      title: '加载中',
+    })
+    let that = this, params = {};
+    Object.assign(params, {
+      key: globalData.key,
+      location:globalData.cityId,
+      type: 0
+    });
+    utils.requestAjax.get('https://devapi.qweather.com/v7/indices/1d', params)
+      .then((res) => {
+        console.log(res)
+        wx.hideLoading();
+        if (res.data.code != 200) {
+          return
+        }
+        that.setData({
+          lifeList: res.data.daily
+        })
+      })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.init();
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
